645b11a98395fa7a924d79a1367fa7b1a0c836e3
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-19 21:56:29 +0530

    print the pattern

---
+++ Assignment_22/Q1_Assignment_22.c
@@ -0,0 +1,29 @@
+/*Accept number from user and print below pattern.
+Input : 5
+Output : A    B   C   D   E
+*/
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1, ch = 'a'; iCnt <= iNo; iCnt++,ch++)
+    {
+        printf("%c\t", ch);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
