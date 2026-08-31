449ab18b60be903817f805198a44216abe90bb9a
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-31 23:12:41 +0530

    write a program which accept string from user and Convert it into upper case.

---
+++ Assignment_32/Q2_Assignment_32.c
@@ -0,0 +1,36 @@
+/*
+write a program which accept string from user and Convert it into upper case.
+
+Input : "Marvellous Multi OS"
+Output : "MARVELLOUS MULTI OS"
+
+*/
+
+#include<stdio.h>
+
+void StrUprx(char *str)
+{
+    int i = 0;
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n']s", Arr);
+
+    StrUprx(Arr);
+
+    printf("Modified String is %s", Arr);
+
+    return 0;
+}
