d4c728e834ca3ef3103d468f98eaa7626e54cdd7
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-31 23:11:36 +0530

    write a program which accept string from user and Convert it into lower case.

---
+++ Assignment_32/Q1_Assignment_32.c
@@ -0,0 +1,36 @@
+/*
+write a program which accept string from user and Convert it into lower case.
+
+Input : "Marvellous Multi OS"
+Output : "marvellous multi os"
+
+*/
+
+#include<stdio.h>
+
+void Strlwex(char *str)
+{
+    int i = 0;
+     
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+    }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n']s", Arr);
+
+    Strlwex(Arr);
+
+    printf("Modified String is : %s ", Arr);
+
+    return 0;
+}
