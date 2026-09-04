91f2d78fff5152611ad09b46c53d2a3a9015cf77
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-09-05 00:09:57 +0530

    Display only odd elements

---
+++ Assignment_36/Q1_Assignment_36.c
@@ -0,0 +1,171 @@
+/*
+Display only odd elements
+print only odd elements
+
+void DisplayOdd(PPNODE first)
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int Data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | ->",first -> Data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE) malloc(sizeof (NODE));
+
+    newn -> Data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE) malloc(sizeof (NODE));
+
+    newn -> Data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        temp -> next = newn;
+    }
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+    int iCount = 0;
+    int i = 0;
+
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    iCount = Count(*first);
+
+    if((iPos < 1) || (iPos > iCount + 1))
+    {
+        printf("Invalid Position");
+        return;
+    }
+    
+    if(iPos == 1)
+    {
+        InsertFirst(first, iNo);
+    }
+    else if(iPos == iCount + 1)
+    {
+        InsertLast(first, iNo);
+    }
+    else
+    {
+        newn = (PNODE) malloc(sizeof (NODE));
+
+        newn -> Data = iNo;
+        newn -> next = NULL;
+        temp = *first;
+        
+        for(i = 0; i < iPos - 1; i++)
+        {
+            temp = temp -> next;
+        }
+
+        newn -> next = temp -> next;
+        temp -> next = newn;
+    }
+}
+
+void DisplayOdd(PPNODE first)
+{
+    while(*first != NULL)
+    {
+        if(((*first) -> Data % 2) != 0)
+        {
+            printf("%d\n",(*first) -> Data);
+        }
+    
+        *first = (*first) -> next;
+    }
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iRet = 0;
+    int iResult = 0;
+
+    InsertFirst(&Head, 51);
+    InsertFirst(&Head, 22);
+    InsertFirst(&Head, 10);
+
+    InsertLast(&Head, 101);
+    InsertLast(&Head, 111);
+
+    InsertAtPos(&Head, 105, 4);
+
+    Display(Head);
+
+    iRet = Count(Head);
+
+    printf("Number of elements are : %d\n",iRet);
+
+    DisplayOdd(&Head);
+
+    return 0;
+}
